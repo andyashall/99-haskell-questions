f64f8d3d82cc5582f004d3a1130966968fc8da3d
andy <andy@ashall.co>
2017-09-08 12:57:10 +0100

    lists done

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,18 @@ module Main where
 
 import Lib
 
+import Data.List (group)
+
+data NestedList a = Elem a | List [NestedList a]
+
 main :: IO ()
 main = do
   let s = myNth ["4", "4", "2", "1", "8", "hello"] 2
   let i = revL ["4", "4", "2", "1", "8", "hello"]
   let p = isPal [1,2,2,1]
-  print p
+  let f = flat (List [Elem 5, List [Elem 4, List [Elem 7, Elem 2], Elem 9]])
+  let c = leng [1,1,1,3,3,3,4,4,4,5]
+  print c
 
 -- 1. Get the last item in a list
 myLast :: [a] -> a
@@ -36,3 +42,24 @@ revL x = reverse x
 -- 6. Is a list a palindrome
 isPal :: (Eq a) => [a] -> Bool
 isPal x = x == (reverse x)
+
+-- 7. Flatten nested list
+flat :: NestedList a -> [a]
+flat (Elem x) = [x]
+flat (List x) = concatMap flat x
+
+-- 8. Remove duplicated elements
+compress :: (Eq a) => [a] -> [a]
+compress = map head . group
+
+-- 9. Pack consecutive duplicates of list elements into sublists
+pack :: (Eq a) => [a] -> [[a]]
+pack [] = []
+pack [x] = [[x]]
+pack (x:xs) = if x `elem` (head (pack xs))
+              then (x:(head (pack xs))):(tail (pack xs))
+              else [x]:(pack xs)
+
+-- 10. Run-length encoding of a list
+leng :: (Eq a) => [a] -> [(Int, a)]
+leng xs = map (\x -> (length x,head x)) (group xs)
